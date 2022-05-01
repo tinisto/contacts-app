8c2b03fbe83cc93a343adc4e32d3f807aec6d548
tinisto <tinisto@gmail.com>
2022-05-01 17:52:25 -0500

    fetch data to Single Contact

--- client/src/components/contacts/SingleContact.js
+++ client/src/components/contacts/SingleContact.js
@@ -1,7 +1,18 @@
 const SingleContact = ({ contact }) => {
   return (
     <>
-      <div className="col-md-6">{contact.id}</div>
+      <div className="col-md-6">
+        <div className="card">
+          <div className="card-body">
+            <p>
+              First Name: <span className="fw-bold">{contact.first_name}</span>
+            </p>
+            <p>
+              Last Name: <span className="fw-bold">{contact.last_name}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
