350ca1f3bb479c96be33d6a68f2546501c3c92ae
tinisto <tinisto@gmail.com>
2022-05-01 16:52:17 -0500

    create GetByID request to DB

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -5,9 +5,11 @@ const {
   createContact,
   deleteContact,
   updateContact,
+  getContactById,
 } = require("../controllers/contact.js")
 
 router.get("/", getAllContacts)
+router.get("/:id", getContactById)
 router.post("/", createContact)
 router.delete("/:id", deleteContact)
 router.put("/:id", updateContact)
