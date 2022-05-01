a3f13c0a8cde8ecc4fee369e7daa3fcba7fd7f9e
tinisto <tinisto@gmail.com>
2022-04-30 21:01:34 -0500

    connect mysqlWorkbench and index.js

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3001
 const cors = require("cors")
+const db = require("./models")
 
 app.use(express.json())
 app.use(express(urlencoded({ extended: true })))
 app.use(cors())
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`)
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+  })
 })
