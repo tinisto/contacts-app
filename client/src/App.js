f59ad733400046ee8e55133dccc5b78f56967b7c
tinisto <tinisto@gmail.com>
2022-05-01 17:30:43 -0500

    create link to NewComponent

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage"
 import ContactPage from "./pages/ContactPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import Layout from "./components/layout/Layout"
+import NewContact from "./components/contacts/NewContact"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/contacts" element={<ContactPage />} />
+          <Route path="/contacts/new" element={<NewContact />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Route>
--- client/src/components/contacts/AllContacts.js
+++ client/src/components/contacts/AllContacts.js
@@ -1,5 +1,7 @@
 import { fetchAllContacts } from "../../api/index.js"
 import { useState, useEffect } from "react"
+import { BsPlusCircle } from "react-icons/bs"
+import { Link } from "react-router-dom"
 
 const AllContacts = () => {
   const [contacts, setContacts] = useState([])
@@ -15,6 +17,26 @@ const AllContacts = () => {
   return (
     <>
       <pre>{JSON.stringify(contacts, null, 2)}</pre>
+      <section className="p-3">
+        <div className="containter">
+          <div className="row">
+            <div className="col">
+              <p className="h1 text-center fw-bold text-primary">
+                Contact Management{" "}
+                <Link to="new" className="btn btn-primary">
+                  <BsPlusCircle className="me-2" />
+                  New
+                </Link>
+              </p>
+              <p className="fst-italic">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto,
+                quas facilis. Ipsam consequuntur esse fugiat enim error modi
+                qui, consequatur magni veritatis velit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
