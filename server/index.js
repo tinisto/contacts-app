368a643c8bb36f0e272376ab0d4bdc1e74210e47
tinisto <tinisto@gmail.com>
2022-04-30 21:44:46 -0500

    create contact router

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ app.use(express.json())
 app.use(express(urlencoded({ extended: true })))
 app.use(cors())
 
+//routes
+const ContactRouter = require("./routes/contacts.js")
+app.use("/contacts", ContactRouter)
+
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
