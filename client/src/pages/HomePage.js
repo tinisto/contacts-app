a069df1d9fa8a06e791c8d7c032c6398687c780c
tinisto <tinisto@gmail.com>
2022-05-01 22:33:39 -0500

    make Home Page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,23 @@
+import { Link } from "react-router-dom"
+
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <>
+      <section className="p-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <p className="h1">CRUD App</p>
+              <p>
+                This <Link to="/contacts"> full stuck project</Link> done with
+                React + Node.js + Express + MySQL
+              </p>
+              <Link to="https://github.com/tinisto/contacts-app">GitHub</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 export default HomePage
