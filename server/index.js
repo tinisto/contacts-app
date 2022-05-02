6bcefa85e2d48797e8ed3ace17d16b82c9431ddc
tinisto <tinisto@gmail.com>
2022-05-01 23:37:42 -0500

    able to connect heroku

--- server/index.js
+++ server/index.js
@@ -5,10 +5,18 @@ const cors = require("cors")
 const db = require("./models")
 const path = require("path")
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"))
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"))
+  })
+}
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use("/public", express.static(path.join(__dirname, "public")))
+
 //routes
 const ContactRouter = require("./routes/contacts.js")
 app.use("/contacts", ContactRouter)
